c50533196325b3d3bc41715a0ab3a0226ce55e6f
Shivam Srivastava <shivsri002@gmail.com>
2026-05-18 00:01:38 +0530

    Updated Climbing Stairs solution

--- 1D_DP/LC_70_Climbing_Stairs.cpp
+++ 1D_DP/LC_70_Climbing_Stairs.cpp
@@ -1,21 +1,77 @@
 // You are climbing a staircase. It takes n steps to reach the top.
-
 // Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
- 
-
 // Example 1:
-
 // Input: n = 2
 // Output: 2
 // Explanation: There are two ways to climb to the top.
 // 1. 1 step + 1 step
 // 2. 2 steps
-// Example 2:
 
+// Example 2:
 // Input: n = 3
 // Output: 3
 // Explanation: There are three ways to climb to the top.
 // 1. 1 step + 1 step + 1 step
 // 2. 1 step + 2 steps
 // 3. 2 steps + 1 step
+
+
+
+//top-down
+//time - o(2 * n+1) => o(n)
+//space = o(n+1 + n ) => o(n)
+
+class Solution {
+public:
+
+
+    int solve( vector<int> &dp,int n)
+    { 
+        if(n<0)
+        return 0;
+        if (n==0)
+        return 1;
+
+        if(dp[n] !=-1)
+        return dp[n];
+
+        int step1 = solve(dp,n-1);
+        int step2 = solve(dp,n-2);
+
+        return dp[n] = step1 + step2;
+
+    }
+    int climbStairs(int n) {
+
+        vector<int>dp(n+1,-1);
+        return solve(dp,n);
+        
+    }
+};
+
+
+
+//bottom up
+//time
+//space
+class Solution {
+public:
+    int solve(int n)
+    { 
+        if(n==0)
+        return 0;
+        vector<int>dp(n+1,-1);
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i =2;i<=n;i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+    int climbStairs(int n) {
+        return solve(n);
+    }
+};
