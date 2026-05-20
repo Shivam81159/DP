ef193e986b68bb1e95f8532d6cb2cb0df2edd99d
Shivam Srivastava <shivsri002@gmail.com>
2026-05-20 18:35:24 +0530

    solved maximize cut segment

--- 1D_DP/GFG_Maximize The Cut Segments.cpp
+++ 1D_DP/GFG_Maximize The Cut Segments.cpp
@@ -72,3 +72,39 @@ class Solution {
         return ans;
     }
 };
+
+
+
+//bottom up
+//tc 
+//sc
+class Solution {
+public:
+    int solve(int n, int x, int y, int z)
+    {
+        vector<int> dp(n + 1, -1);
+
+        dp[0] = 0;
+
+        for(int i = 1; i <= n; i++)
+        {
+            if(i - x >= 0 && dp[i - x] != -1)
+                dp[i] = max(dp[i], dp[i - x] + 1);
+
+            if(i - y >= 0 && dp[i - y] != -1)
+                dp[i] = max(dp[i], dp[i - y] + 1);
+
+            if(i - z >= 0 && dp[i - z] != -1)
+                dp[i] = max(dp[i], dp[i - z] + 1);
+        }
+
+        return dp[n];
+    }
+
+    int maximizeTheCuts(int n, int x, int y, int z) {
+        int ans = solve(n, x, y, z);
+
+        return max(ans, 0);
+    }
+};
+
